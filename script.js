69f6f5e12dd7695a1e791dd1f88edf02a1336228
JultomtenMedA <Andreas.sandgren@elev.ga.ntig.se>
2023-02-15 15:53:53 +0100

    yup
    Restart

--- script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $("#search-btn").click(function() {
-      var searchTerm = $("#search-box").val();
-      alert("Searching for: " + searchTerm);
-    });
-  });
-  
